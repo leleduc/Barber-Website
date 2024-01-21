d2c2d113e9805416f973efa0b5b47a284f0d4e5e
leleduc <leleduc@gmail.com>
2024-01-21 22:00:35 +0700

    Finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,52 @@ addEventOnElem(navToggler, 'click', toggleNavbar);
 const closeNavbar = () => navbar.classList.remove('active');
 
 addEventOnElem(navLinks, 'click', closeNavbar);
+
+/**
+ * header & back top btn active when scroll down to 100px
+ */
+
+const header = document.querySelector('[data-header]');
+const backTopBtn = document.querySelector('[data-back-top-btn]');
+
+const headerActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add('active');
+    backTopBtn.classList.add('active');
+  } else {
+    header.classList.remove('active');
+    backTopBtn.classList.remove('active');
+  }
+};
+
+addEventOnElem(window, 'scroll', headerActive);
+
+/**
+ * filter function
+ */
+
+const filterBtns = document.querySelectorAll('[data-filter-btn]');
+const filterItems = document.querySelectorAll('[data-filter]');
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove('active');
+  this.classList.add('active');
+  lastClickedFilterBtn = this;
+
+  for (let i = 0; i < filterItems.length; i++) {
+    if (
+      this.dataset.filterBtn === filterItems[i].dataset.filter ||
+      this.dataset.filterBtn === 'all'
+    ) {
+      filterItems[i].style.display = 'block';
+      filterItems[i].classList.add('active');
+    } else {
+      filterItems[i].style.display = 'none';
+      filterItems[i].classList.remove('active');
+    }
+  }
+};
+
+addEventOnElem(filterBtns, 'click', filter);
